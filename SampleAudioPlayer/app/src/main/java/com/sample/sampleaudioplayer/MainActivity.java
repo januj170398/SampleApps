9d4cbe90bc0b3000e5d19a446d1faa57e1eb6887
sciage <dass.harish@gmail.com>
2020-01-13 16:58:29 +0530

    Working android service class audio play

--- SampleAudioPlayer/app/src/main/java/com/sample/sampleaudioplayer/MainActivity.java
+++ SampleAudioPlayer/app/src/main/java/com/sample/sampleaudioplayer/MainActivity.java
@@ -1,11 +1,18 @@
 package com.sample.sampleaudioplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -13,7 +20,33 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    PlayerService mBounderService;
+    boolean mServiceBound = false;
+
     static FloatingActionButton playPauseButton;
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            boolean isPlaying = intent.getBooleanExtra("isPlaying", false);
+            flipPlayPauseButton(isPlaying);
+        }
+    };
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            PlayerService.MyBinder myBinder = (PlayerService.MyBinder) service;
+            mBounderService =  myBinder.getService();
+            mServiceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +59,51 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if (mServiceBound){
+                    mBounderService.togglePlayer();
+                }
+
              //   Player.player.togglePlayer();
             }
         });
 
         String url = "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3";
 
-        Intent i = new Intent(this, PlayerService.class);
-        i.putExtra("url", url);
-        startService(i);
+
+        startStreamingService(url);
 
        // if (Player.player == null){
        //     new Player();
           //  Player.player.playStream(url);
        // }
+    }
+
+    private void startStreamingService(String url){
+        Intent i = new Intent(this, PlayerService.class);
+        i.putExtra("url", url);
+        startService(i);
+        bindService(i, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mServiceBound){
+            unbindService(mServiceConnection);
+            mServiceBound = false;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter("changePlayButton"));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
     }
 
     public static void flipPlayPauseButton (boolean isPlaying){
--- SampleAudioPlayer/app/src/main/java/com/sample/sampleaudioplayer/PlayerService.java
+++ SampleAudioPlayer/app/src/main/java/com/sample/sampleaudioplayer/PlayerService.java
@@ -4,12 +4,23 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import java.io.IOException;
 
 public class PlayerService extends Service {
+
+    private final IBinder mBinder = new MyBinder();
+
+    public class MyBinder extends Binder{
+        PlayerService getService(){
+            return PlayerService.this;
+        }
+    }
 
     MediaPlayer mediaPlayer = new MediaPlayer();
 
@@ -19,12 +30,14 @@ public class PlayerService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        return null;
+        return mBinder;
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        playStream(intent.getStringExtra("url"));
+        if (intent.getStringExtra("url") != null){
+            playStream(intent.getStringExtra("url"));
+        }
 
         return START_NOT_STICKY; // service will only run when it is processing commands
     }
@@ -54,7 +67,8 @@ public class PlayerService extends Service {
             mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                 @Override
                 public void onCompletion(MediaPlayer mp) {
-                    MainActivity.flipPlayPauseButton(false);
+                  //  MainActivity.flipPlayPauseButton(false);
+                    flipPlayPauseButton(false);
                 }
             });
             mediaPlayer.prepareAsync();
@@ -67,7 +81,8 @@ public class PlayerService extends Service {
     public void pausePlayer(){
         try {
             mediaPlayer.pause();
-            MainActivity.flipPlayPauseButton(false);
+           // MainActivity.flipPlayPauseButton(false);
+            flipPlayPauseButton(false);
 
         } catch (Exception e){
             Log.d("PLAYER", "pausePlayer: failed to pause");
@@ -77,11 +92,20 @@ public class PlayerService extends Service {
     public void playPlayer(){
         try {
             mediaPlayer.start();
-            MainActivity.flipPlayPauseButton(true);
+            // MainActivity.flipPlayPauseButton(true);
+            flipPlayPauseButton(true);
 
         } catch (Exception e){
             Log.d("PLAYER", "pausePlayer: failed to pause");
         }
+    }
+
+    public void flipPlayPauseButton(boolean isPlaying){
+        Intent intent = new Intent("changePlayButton");
+        intent.putExtra("isPlaying", isPlaying);
+
+        // Broadcast receiver
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 
     public void togglePlayer(){
